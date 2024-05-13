1fafd2513df6184a9a9854e832270826c9a6aaea
msalaz03 <116814238+msalaz03@users.noreply.github.com>
2024-05-13 18:35:56 -0400

    testing entry point scripts

---
+++ servo_controller/servo_controller/print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rclpy #import ros client library such as nodes and topics
+from rclpy.node import Node #import specifically node
+
+from std_msgs.msg import String
+from std_msgs.msg import Int32
+
+def main(args=None):
+    print("HELLLO")
+
+
+if __name__ == '__main__':
+    main()
